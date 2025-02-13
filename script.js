b17384cf03ee5749a06158f426155a95e9a22bd3
akshadity01 <akshaykumaraditya01@gmail.com>
2025-02-13 22:10:13 +0530

    have some styling

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-console.log("hello")
-
-
 setInterval(() => {
     fetch(`https://www.youtube.com/channel/UC7GOzVmO24_B8M0izj2-EsQ/live`)
-    .then(response => {
-        if (response.url.includes("watch?v=")) {
-            document.getElementById("videoFrame").src = response.url.replace("watch?v=", "embed/");
-        } else {
-            document.getElementById("videoFrame").src = "https://www.youtube.com/embed/W78hJ9XDcLc?si=VTwuWfiyEoQFTxf6";
-        }
-    });
-}, 60000); // Check every 60 seconds 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.url;
+        })
+        .then(url => {
+            if (url.includes("watch?v=")) {
+                document.getElementById("videoFrame").src = url.replace("watch?v=", "embed/");
+            } else {
+                document.getElementById("videoFrame").src = "https://www.youtube.com/embed/W78hJ9XDcLc?si=VTwuWfiyEoQFTxf6";
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching live stream URL:", error);
+        });
+}, 90000); // Check every 60 seconds
